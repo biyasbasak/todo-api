bd63d9cd6994169ecdddbf6784d3b9df5082c4ba
Biyas Basak <basakbiyas001@gmail.com>
2016-02-28 02:33:09 +0530

    Add Post for todos

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 var expres = require('express');
+var bodyParser = require('body-parser');
+
 var app = expres();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'go to college',
-	completed: false
-}, {
-	id: 2,
-	description: 'complete lab work',
-	completed: false
-}, {
-	id: 3,
-	description: 'sleep',
-	completed: false
-}];
+var todos = [];
+var todoNextId = 1; //increments id by 1
+
+//express middleware 
+//json requests comes in it express pareses it and we can access it via request.body
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('TODO API ROOT');
@@ -23,7 +18,7 @@ app.get('/todos', function(req, res) {
 	res.json(todos);
 });
 app.get('/todos/:id', function(req, res) {
-	var todoId = parseInt(req.params.id, 10);  // req is always in string so convert it to int
+	var todoId = parseInt(req.params.id, 10); // req is always in string so convert it to int 
 	var matchedtodo;
 
 	todos.forEach(function(todos) {
@@ -37,6 +32,16 @@ app.get('/todos/:id', function(req, res) {
 	} else {
 		res.status(404).send();
 	}
+});
+
+// it can take data, send an json object along with the request and server takes the json and stores in the todos
+//id is not added as it is generated after its added to the array
+app.post('/todos', function(req, res) {
+	var body = req.body;
+	body.id = todoNextId++;
+	todos.push(body);
+	res.json(body);
+
 });
 
 
